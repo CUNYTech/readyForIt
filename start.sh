d6f7e806de10376f19251d782773713675703f4a
marcuslkc <marcuscrowder66@gmail.com>
2018-03-16 11:28:14 -0400

    merged for the purge

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -v
+set -x
+export FLASK_APP=auto_run.py
+export FLASK_DEBUG=0
+concurrently -n "React,Flask" -c "bgBlue.bold,bgMagenta.bold" "npm run start --prefix front_end/" "flask run"
+set +x
