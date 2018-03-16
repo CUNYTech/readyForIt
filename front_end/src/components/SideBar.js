2c049374c0c8e62fcbb285ce72ff84b6799819fa
Alexandra Havia <sasha.havia@gmail.com>
2018-03-16 11:43:30 -0400

    Side bar completed

--- front_end/src/components/SideBar.js
+++ front_end/src/components/SideBar.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 
 import {slide as Menu} from 'react-burger-menu';
+
 import '../css/SideBar.css';
 
 
 class SideBar extends Component {
-    
     render () {
         return (
-            
             <Menu className="menu">
                 <h1>ReadyForIt</h1>
+                <a id="weather" href=""><i className="fa fa-fw fa-cloud"></i><span>Weather Updates</span></a>
+                <a id="donations" href=""><i className="fa fa-fw fa-ambulance"></i><span>Donations</span></a>
+                <a id="people-say" href=""><i className="fa fa-fw fa-comments"></i><span>People Say</span></a>
+                <a id="statistics" href=""><i className="fa fa-fw fa-history"></i><span>Statistics</span></a>
                 
-                <a id="weather" className="menu-item" href="/weather"><i className="fa fa-fw fa-cloud"></i><span>Weather Updates</span></a>
-                <a id="donations" className="menu-item" href="/donations"><i className="fa fa-fw fa-ambulance"></i><span>Donations</span></a>
-                <a id="people-say" className="menu-item" href="/people-say"><i className="fa fa-fw fa-comments"></i><span>People Say</span></a>
-                <a id="statistics" className="menu-item" href="/statistics"><i className="fa fa-fw fa-history"></i><span>Statistics</span></a>
+                <a id="sign-up" href="">Sign Up</a>
+                
+                <div id="social">
+                    <a href="/facebook" className="fa fa-fw fa-facebook"></a>
+                    <a href="/twitter" className="fa fa-fw fa-twitter"></a>
+                    <a href="/instagram" className="fa fa-fw fa-instagram"></a>
+                    <p>Copyright @ 2018 ReadyForIt. All rights reserved.</p>
+                </div>
             </Menu>
         );
     }
