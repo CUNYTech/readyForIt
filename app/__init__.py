f2cd13edeca0012fc187128f39f3f2d903616473
marcuslkc <marcuscrowder66@gmail.com>
2018-03-17 20:55:33 -0400

    Build now works as expected

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 
 def create_app(config):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="build", static_folder="build/static")
     app.config.from_object(config)
     db.init_app(app)
     from app.views.main import main as main_blueprint
@@ -14,7 +14,24 @@ def create_app(config):
     from app.api_v_1.users import api as api_users
     app.register_blueprint(api_users)
 
+    # Temporary fix until Nginx
+
+    @app.route('/index.html')
+    def serve_html():
+        return send_from_directory('build', 'index.html')
+
+    
+    @app.route('/service-worker.js')
+    def serve_worker():
+        return send_from_directory('build', 'service-worker.js')
+    
+    @app.route('/favicon.ico')
+    def serve_fav():
+        return send_from_directory('build', 'favicon.ico')
+
+    
     from app import models
+
 
     return app
 
