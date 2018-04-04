82a0ad02dd269aeb894fcca9ce5b85ff100385ca
nick <nickhidalgo93@gmail.com>
2018-04-04 18:23:48 -0400

    fixed sidebar css

--- front_end/src/components/CurrentWeather.js
+++ front_end/src/components/CurrentWeather.js
@@ -8,16 +8,21 @@ import { getGeoLocation } from '../helpers/geolocation';
 class CurrentWeather extends Component {
   state = {
     lon: "-73.935242",
-    lat: "40.730610"
+    lat: "40.730610",
+    type: "geo"
   }
 
-  componentDidMount(){
+  componentWillMount(){
     getGeoLocation().then(latlng => {
       this.setState({
         lat: latlng[0].toFixed(10),
         lon: latlng[1].toFixed(10)
       });
-    }).catch(error=> console.error(error));
+    }).catch(error=> {
+      this.setState({
+        type: "auto"
+      })
+    });
   } 
   
   render() {   
@@ -28,9 +33,9 @@ class CurrentWeather extends Component {
         apikey="f0c751300d9340ac8c9202734182503 "
         // type="city"
         // city="Manhattan"
-        type="auto"
-        //lon={this.state.lon}
-        //lat={this.state.lat}
+        type={this.state.type}
+        lon={this.state.lon}
+        lat={this.state.lat}
     />     
     );
   }  
