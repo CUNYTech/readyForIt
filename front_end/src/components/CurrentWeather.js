6cb7f4351f1bdfd880c949b2f8f5fdbeb761cb78
de-john <dewitt.john@baruchmail.cuny.edu>
2018-04-19 20:02:21 -0400

    added hourly weather updates

--- front_end/src/components/CurrentWeather.js
+++ front_end/src/components/CurrentWeather.js
@@ -4,49 +4,81 @@ import moment from 'moment';
 import { getGeoLocation } from '../helpers/geolocation';
 import WeatherIcon from 'react-open-weather-icons';
 import 'react-open-weather/lib/css/ReactWeather.css';
+import axios from 'axios';
+import HourlyWeather from './HourlyWeather';
 
 class CurrentWeather extends Component {
-  state = {
-    lon: "-73.935242",
-    lat: "40.730610",
-    type: "geo",
-
+  constructor(props){
+    super(props)
+    this.state = {
+      LatLng: {
+        lon: "-73.935242",
+        lat: "40.730610",
+        type: "geo",
+      },
+      // HourlyWeather: []
+    };
   }
+  // componentDidMount() {
+  //   axios.get('http://api.wunderground.com/api/bfb68c7fd935c550/hourly/q/CA/San_Francisco.json')
+  //   .then(response => this.setState({HourlyWeather: response.data.hourly_forecast}))
+  //   .catch(this.setState({HourlyWeather: []}));
+  //   // .then(response => {
+  //   //   const hourForecast = response.data.hourly_forecast.map(hourInc => {
+  //   //   });
+  //   //   this.setState({
+  //   //     HourlyWeather: response.data.hourly_forecast
+  //   //   });  
+  //   // });
+  // }
+
+
 
   componentWillMount(){
     getGeoLocation().then(latlng => {
       this.setState({
-        lat: latlng[0].toFixed(10),
-        lon: latlng[1].toFixed(10),
-
+        LatLng: {
+          lat: latlng[0].toFixed(10),
+          lon: latlng[1].toFixed(10),
+        }
       });
     }).catch(error=> {
       this.setState({
-        type: "auto"
+        LatLng: {
+          type: "auto"
+        }
       })
     });
   }
 
   render() {
-    return (
+   
+    // <li key={i}>Time: {hour.FCTTIME.hour}</li>
+      //  <li key={i}>TIME: {hour.FCTTIME.hour} Temperature: {hour.temp} Condition: {hour.condition}</li>
       
+      
+    return (
+      <div>
       <ReactWeather
         forecast="today"
         unit="imperial"  
         apikey="f0c751300d9340ac8c9202734182503"
         icon="01d"
         name="04d"
-        type={this.state.type}
-        lon={this.state.lon}
-        lat={this.state.lat}
+        type={this.state.LatLng.btype}
+        lon={this.state.LatLng.lon}
+        lat={this.state.LatLng.lat}
         forecast="5days"    
     />
-    
+      <HourlyWeather/>
+     </div>
     );
-  
-     {/*<WeatherIcon name="01d" className="my-awesome-icon" />*/}
    
   }  
 }
 
 export default CurrentWeather;
+
+  //  this.state.HourlyWeather.map(function(hour, i){
+  //           return <li key={i}>{hour.}</li>
+  //         }) 
---
+++ front_end/src/components/HourlyWeather.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import '../css/Weather.css';
+
+class HourlyWeather extends React.Component {
+    constructor(){
+    super()
+    this.state = {
+        HourlyWeather: []
+        }
+    }
+    
+    componentDidMount() {
+        axios.get('http://api.wunderground.com/api/bfb68c7fd935c550/hourly/q/NY/New_York.json')
+        .then(response => {
+            const hourlyData = response.data.hourly_forecast;
+            this.setState({HourlyWeather: response.data.hourly_forecast});
+            });
+        }
+      
+    render() {
+            const outputData = this.state.HourlyWeather;
+        return (
+            <div>
+                <h2>Hourly Weather</h2>
+                <table className="center">
+                    <tr>
+                        <th>Time</th>
+                        <th>Temperature</th>
+                        <th>Condition</th>
+                        <th>Wind Speed</th>
+                        <th>Feels Like</th>
+                    </tr>
+                        {outputData.map(function(hour, i){
+                            return (
+                                <tr>
+                                    <td key={i}>{hour.FCTTIME.pretty}</td> 
+                                    <td key={i}>{hour.temp.english}</td> 
+                                    <td key={i}>{hour.condition}<img src={hour.icon_url}/></td> 
+                                    <td key={i}>{hour.wdir.dir} @ {hour.wspd.english}</td>
+                                    <td key={i}>{hour.feelslike.english}</td>
+                                </tr>
+                            )
+                            })}
+                </table>
+            </div>
+        )
+    }
+    
+}
+
+export default HourlyWeather;
