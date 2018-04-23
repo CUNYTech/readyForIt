7050c4162e55287432a3b1c65a5be0aa608d8eab
de-john <dewitt.john@baruchmail.cuny.edu>
2018-04-23 19:43:02 -0400

    updated hourly weather

--- front_end/src/components/App.js
+++ front_end/src/components/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Map from './Map';
-import CurrentWeather from './CurrentWeather';
-//import logo from '../css/logo.svg';
 import '../css/App.css';
 
 
